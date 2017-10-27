a3b5a84304b1f8bf020fd4bfffff73398a59f433
jackfinal <final_m@foxmail.com>
2017-10-27 17:24:56 +0800

    Signed-off-by: jackfinal <final_m@foxmail.com>

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,16 @@
 //index.js
+var WxParse = require('../../wxParse/wxParse.js');
 //获取应用实例
 var app = getApp()
 Page({
   data: {
     BaseInfo: {},
+    close: 0,
+    block: 'none',
+    topBackground: '',
+    BaseImgUrl: app.BaseImgUrl,
+    //页面内
+    otherList: [1],
     imgUrls: [
       app.BaseImgUrl + '/templets/szjdwap/images/banner1.jpg',
       app.BaseImgUrl + '/templets/szjdwap/images/banner2.jpg',
@@ -12,9 +19,7 @@ Page({
     indicatorDots: true,
     autoplay: false,
     interval: 5000,
-    duration: 1000,
-    close: 0,
-    block: 'none'
+    duration: 1000
   },
   onLoad: function () {
     var that = this
@@ -24,6 +29,10 @@ Page({
     wx.setNavigationBarTitle({
       title: app.BaseInfo.cfg_webname
     })
+    
+  },
+  onShow: function() {
+    this.getOther();
   },
   onClickMenu: function () {
     if (this.data.close === 0) {
@@ -38,6 +47,34 @@ Page({
       })
     }
     console.log(this.data.close);
+  },
+  onPageScroll: function (e) {
+    if (e.scrollTop > 112) {
+      this.setData({
+        topBackground: 'topBackground'
+      })
+    } else if (e.scrollTop == 0) {
+      this.setData({
+        topBackground: ''
+      })
+    }
+  },
+  getOther: function() {
+    var that = this;
+    app.RequestUrl('/index.php/index/getOther', {}, '', function (res) {
+      if (res.data.status == 1) {
+        that.setData({
+          otherList: res.data.data
+        })
+      }else {
+        wx.showToast({
+          title: '加载失败',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+      WxParse.wxParse('article', 'html', that.data.otherList.about, that, 5);
+      console.log(that.data.otherList);
+    });
   }
-  
 })
